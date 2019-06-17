8555a73a766d6517ddf4e8b9479c96e4372c4ea2
Aruncsk <arunsubburaj@gmail.com>
2019-06-17 18:16:35 +0530

    Clasification Regressions

---
+++ ClassificationAlgorithms/LoadModel.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import pickle
+from sklearn.preprocessing import StandardScaler
+
+file_name = "LogisticRegression.pkl"
+pkl_file = open(file_name, 'rb')
+model_pkl = pickle.load(pkl_file)
+
+#Load Data
+data = pd.read_csv("Social_Network_Ads.csv")
+x_test = data.iloc[:,2:3].values
+y_test = data.iloc[:,4].values
+
+sc_X = StandardScaler()
+x = sc_X.fit_transform(x_test)
+
+y_pred_pkl = model_pkl.predict(x_test)
+#print(x_test)
+
+#print(y_pred_pkl)
