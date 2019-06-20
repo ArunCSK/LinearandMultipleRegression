6d4a4b42c3d9c049e0945bbe3499cbd7cd96f6a9
Aruncsk <arunsubburaj@gmail.com>
2019-06-20 15:16:08 +0530

    Logistic model for HIV DataSet

--- ClassificationAlgorithms/LogisticRegression.py
+++ ClassificationAlgorithms/LogisticRegression.py
@@ -1,12 +1,9 @@
 #import libraries
-import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.model_selection  import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix
-from matplotlib.colors import ListedColormap
 import pickle
 
 #Load Data
@@ -18,12 +15,13 @@ print(X)
 
 #Split Data
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25 , random_state = 0 )
-#print(X_test)
+
 #feature scaling
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 #print(X_test)
+
 #prediction
 classifier = LogisticRegression()
 classifier.fit(X_train, y_train)
