1f3dc4c51a87d63f60028d8b7783b7e6685f4947
Aruncsk <arunsubburaj@gmail.com>
2019-07-19 16:55:46 +0530

    week six review

---
+++ HDFS/week6_review.py
@@ -0,0 +1,49 @@
+# Databricks notebook source
+import pandas as pd
+import pickle
+from sklearn.linear_model import LinearRegression
+
+dbutils.widgets.text("output", "","")
+dbutils.widgets.get("output")
+FilePath = getArgument("output")
+
+dbutils.widgets.text("filename", "","")
+dbutils.widgets.get("filename")
+filename = getArgument("filename")
+storage_account_name = "week6storage"
+storage_account_access_key = "La0RcP2uBJK2j1mqI2vatAhFs7LfKYpxI4CrHvhd99xQ2ACe7dsoUSRf2DEB66XmGDBeEWQERB9JBXmioP9z8Q=="
+
+
+spark.conf.set(
+ "fs.azure.account.key."+storage_account_name+".blob.core.windows.net",
+ storage_account_access_key)
+
+
+file_location = "wasbs://week6container@week6storage.blob.core.windows.net"+FilePath+"/"+filename
+print(file_location)
+file_type = "csv"
+
+
+df = spark.read.format(file_type).option("inferSchema", "true").load(file_location)
+#df.show()
+
+data = df.toPandas()
+X_train = data.iloc[2:len(data), 2:6]
+y_train = data.iloc[2:len(data), 1]
+
+reg = LinearRegression()
+model = reg.fit(X_train,y_train)
+
+filename = "linear.pkl"
+pklfile = open(filename, 'wb')
+model = pickle.dump(model, pklfile)
+pklfile.close()
+
+
+display(dbutils("files:databricks/driver"))
+
+
+
+# COMMAND ----------
+
+
