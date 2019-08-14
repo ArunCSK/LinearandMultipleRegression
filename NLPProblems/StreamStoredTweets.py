96d040f0202aff19ead9230896ff25634ca08e7a
Aruncsk <arunsubburaj@gmail.com>
2019-08-14 15:33:02 +0530

    add files

--- NLPProblems/StreamStoredTweets.py
+++ NLPProblems/StreamStoredTweets.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 tweets_data_path = '/home/admin1/Desktop/PythonVM/NLPProblems/tweet_data.txt'
-
+data = pd.read_csv('/home/admin1/Desktop/PythonVM/NLPProblems/tweet_data.txt')
 tweets_data = []
 tweets_file = open(tweets_data_path, "r")
 for line in tweets_file:
